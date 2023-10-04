1ed7bac1f3c838b36f0fafb79949812cb970e9d3
Hasan Ayas <ayas.hasan0007@gmail.com>
2023-10-04 14:21:21 +0300

    Week2 Homework

---
+++ Week2/BookStore/Startup.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using BookStore.Middlewares;
+using BookStore.Services;
+using BookStore.Attributes;
+using static System.Net.WebRequestMethods;
+
+namespace BookStore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BookStore", Version = "v1" });
+            });
+
+            // FakeBookService'yi eklemek için yapılandırma
+            services.AddScoped<IBookService, FakeBookService>();
+        }
+
+        //Bu metod, HTTP istemcilerinin gelen isteklere nasıl yanıt vereceğini yapılandırır.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BookStore v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            // RequestLoggingMiddleware ve GlobalExceptionMiddleware
+            app.UseMiddleware<RequestLoggingMiddleware>();
+            app.UseMiddleware<GlobalExceptionMiddleware>();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
