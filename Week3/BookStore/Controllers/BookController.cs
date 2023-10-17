682fc068e78aa01b0d4c27b38473488b64940dee
Hasan Ayas <86951716+hasanayas@users.noreply.github.com>
2023-10-17 12:30:56 +0300

    Week3 Homework - Model Usage

---
+++ Week3/BookStore/Controllers/BookController.cs
@@ -0,0 +1,112 @@
+﻿using BookStore.Attributes;
+using BookStore.Data;
+using BookStore.DbOperations;
+using BookStore.Validators;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.BookOperations.GetBookDetail;
+using BookStore.BookOperations.GetBooks;
+using BookStore.BookOperations.CreateBook;
+using static BookStore.BookOperations.CreateBook.CreateBookCommand;
+using BookStore.BookOperations.DeleteBook;
+using BookStore.BookOperations.UpdateBook;
+using static BookStore.BookOperations.UpdateBook.UpdateBookCommand;
+
+namespace BookStore.Controllers
+{
+    [ApiController]
+    [Route("[controller]s")]
+    public class BookController : ControllerBase
+    {
+        private readonly BookStoreDbContext _context;
+        private static List<Book> BookList = BookData.BookList;
+
+
+        public BookController(BookStoreDbContext context)
+        {
+            _context = context;
+        }
+
+
+        //GET
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+            GetBookQuery query = new GetBookQuery(_context);
+            var result = query.Handle();
+            return Ok(result);
+        }
+
+
+        //GETBYID
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            BookDetailViewModel result;
+            GetBookDetailQuery query = new GetBookDetailQuery(_context);
+            query.BookId = id;
+            result = query.Handle();
+            return Ok(result);
+        }
+
+
+        //CREATE
+        [HttpPost]
+        public IActionResult AddBook([FromBody] CreateBookModel newBook)
+        {
+            CreateBookCommand command = new CreateBookCommand(_context);
+
+            try
+            {
+                command.Model = newBook;
+                command.Handle();
+
+            } catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            return Ok();
+               
+        }
+
+
+        //UPDATE
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, [FromBody] UpdateBookModel updatedBook)
+        {
+            UpdateBookCommand command = new UpdateBookCommand(_context);
+            command.BookId = id;
+            command.Model = updatedBook;
+            command.Handle();
+            return Ok();
+        }
+
+
+        //DELETE
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            DeleteBookCommand command = new DeleteBookCommand(_context);
+            command.BookId = id;
+            command.Handle();
+            return Ok();
+        }
+
+
+        [HttpGet("list")]
+        public List<Book> GetBooksByName([FromQuery] string name)
+        {
+            var bookList = BookList.Where(x => x.Title.Contains(name, StringComparison.OrdinalIgnoreCase)).ToList();
+            return bookList;
+        }
+
+        //Yetkisiz Giriş 
+      
+        [HttpGet("FakeUser")]
+        [AuthorizeFakeUser]
+        public List<Book> GetBooksForFakeUser([FromQuery] string name)
+        {
+            var bookList = BookList.Where(x => x.Title.Contains(name, StringComparison.OrdinalIgnoreCase)).ToList();
+            return bookList;
+        }
+    }
+}
---
+++ Week3/BookStore/Data/BookData.cs
@@ -0,0 +1,59 @@
+﻿
+
+namespace BookStore.Data
+{
+    public static class BookData
+    {
+        public static List<Book> BookList { get; set; } = new List<Book>
+        {
+            new Book
+            {
+                Id = 1,
+                Title = "Lean Startup",
+                GenreId = 1,
+                PageCount = 200,
+                PublishDate = new DateTime(2023,06,12)
+            },
+            new Book
+            {
+                Id = 2,
+                Title = "Herland",
+                GenreId = 1,
+                PageCount = 250,
+                PublishDate = new DateTime(2023,07,12)
+            },
+            new Book
+            {
+                Id = 3,
+                Title = "Dune",
+                GenreId = 3,
+                PageCount = 300,
+                PublishDate = new DateTime(2023,12,21)
+            },
+            new Book
+            {
+                Id = 4,
+                Title = "Harry Potter: Philosopher's Stone",
+                GenreId = 2,
+                PageCount = 223,
+                PublishDate = new DateTime(2023,05,15)
+            },
+            new Book
+            {
+                Id = 5,
+                Title = "Harry Potter: Chamber of Secrets",
+                GenreId = 2,
+                PageCount = 384,
+                PublishDate = new DateTime(2023,05,20)
+            },
+            new Book
+            {
+                Id = 6,
+                Title = "Harry Potter: Prisoner of Azkaban",
+                GenreId = 2,
+                PageCount = 448,
+                PublishDate = new DateTime(2023,05,25)
+            }
+        };
+    }
+}
